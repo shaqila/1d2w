0282a0d20dfc1fa05c84ab92600758abe11bfcba
shaqila <shaqilaerbeliza@gmail.com>
2021-04-19 00:49:36 +0700

    about

--- resources/views/layouts/pages/about.blade.php
+++ resources/views/layouts/pages/about.blade.php
@@ -5,5 +5,40 @@ About | One Day To Write
 @endsection
 
 @section('content')
-
+<section id="about" class="about" data-aos="fade-up" data-aos-duration="2000">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6">
+            <img src="assets/img/about.png" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-6 pt-4 pt-lg-0 content">
+            <h3>One Day To Write (ODTW)</h3>
+            <p class="fst-italic">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+              dolore
+              magna aliqua.
+            </p>
+            <ul>
+              <li><i class="bi bi-check-circle"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat</li>
+              <li><i class="bi bi-check-circle"></i> Duis aute irure dolor in reprehenderit in voluptate velit</li>
+              <li><i class="bi bi-check-circle"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                irure dolor in reprehenderit in voluptate trideta storacalaperda</li>
+            </ul>
+            <p>
+              Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+              voluptate
+              velit esse cillum dolore eu fugiat nulla pariatur.
+            </p>
+          </div>
+          <div class="col-lg-12 pt-4 pt-lg-5 content">
+            <h2>Cara Pendaftaran</h2>
+            <p class="fst-italic">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+              dolore
+              magna aliqua.
+            </p>
+          </div>
+        </div>
+      </div>
+</section>
 @endsection
