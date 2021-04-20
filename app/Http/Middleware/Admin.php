152b88b965bfb255129983d3c087acd1e7ca55d0
shaqila <shaqilaerbeliza@gmail.com>
2021-04-20 21:59:35 +0700

    auth

--- app/Http/Middleware/Admin.php
+++ app/Http/Middleware/Admin.php
@@ -17,7 +17,7 @@ class Admin
     public function handle(Request $request, Closure $next)
     {
         
-        if ($request->user()->level == "peserta") {
+        if ($request->user()->role == "peserta") {
             return redirect('/peserta/dashboard');
         } else {
             return $next($request);
